6958a51663bbce321c6addac477215c7958ba450
BDav1992 <45438374+BDav1992@users.noreply.github.com>
2020-10-29 19:49:21 +0100

    Add files via upload

---
+++ DH1_GP/Buttons.ino
@@ -0,0 +1,21 @@
+void CheckAllButtons(void) {
+  if (buttbx.getKeys())
+  {
+    for (int i = 0; i < LIST_MAX; i++)
+    {
+      if ( buttbx.key[i].stateChanged )
+      {
+        switch (buttbx.key[i].kstate) {
+          case PRESSED:
+          case HOLD:
+            Joystick.setButton(buttbx.key[i].kchar, 1);
+            break;
+          case RELEASED:
+          case IDLE:
+            Joystick.setButton(buttbx.key[i].kchar, 0);
+            break;
+        }
+      }
+    }
+  }
+}
---
+++ DH1_GP/Clutch.ino
@@ -0,0 +1,33 @@
+  //Modify the value if you want to finetune your clutches. 100 means you use only the left clutch.
+  int ClutchLeftPercentage = 50;
+
+  //Left clutch
+  int ClutchLeftReleased = 340;
+  int ClutchLeftPulled = 292;
+
+  //Right clutch
+  int ClutchRightReleased = 556;
+  int ClutchRightPulled = 605;
+
+  //Do not modify this!!!
+  int ClutchRightPercentage = 100-ClutchLeftPercentage;
+  int ClutchLeftValue = (1023/100)*ClutchLeftPercentage;
+  int ClutchRightValue = (1023/100)*ClutchRightPercentage;  
+
+void Clutch(void) {
+  //Left clutch
+  int ClutchLeft = analogRead(A0);
+  ClutchLeft = map(ClutchLeft, ClutchLeftReleased, ClutchLeftPulled, 0, ClutchLeftValue);
+  
+  //Right clutch
+  int ClutchRight = analogRead(A1);
+  ClutchRight = map(ClutchRight, ClutchRightReleased, ClutchRightPulled, 0, ClutchRightValue);
+
+  //Combined clutch
+  int ClutchAll = (ClutchLeft + ClutchRight);
+  Joystick.setZAxis(ClutchAll);
+
+  //Uncomment below to debug.
+  Serial.println(ClutchAll);
+
+}
